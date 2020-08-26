46e75ce91f44f58adfd5f9c9d9ae1c15aaf82790
Daniel Guzman <daniel.guzman@ort.edu.ar>
2020-08-25 23:11:57 -0300

    Cambios finales

--- script.js
+++ script.js
@@ -14,13 +14,56 @@ let totalTareas = 0
 let tareasPendientes = 0
 
 
-function addTodo() {
-  console.log("Aqui viene la magia: ")
+function cambioCheckBox (){
+  console.log(this)
+  if (this.checked){
+    tareasPendientes--;
+  }else{
+    tareasPendientes++;
+  }
+  renderizarContadores()
+}
 
-  // Aqui hago la creacion del elemento
+function crearTarea(name){
   const checkbox = document.createElement('input')
   checkbox.className = classNames.TODO_CHECKBOX
+  // Agrego una funcion callback ("cambiocheckbox") 
+  // al evento onchange de este input
+  checkbox.onchange = cambioCheckBox
   checkbox.type = 'checkbox'
 
-  console.log("elemento checkbox", checkbox)
+  const span = document.createElement('span')
+  span.className = classNames.TODO_TEXT;
+  span.setAttribute("editable", true)
+  span.innerHTML = name
+
+  const li = document.createElement('li')
+  li.className = classNames.TODO_ITEM
+  li.appendChild(checkbox)
+  li.appendChild(span)
+
+  return li
+}
+
+function renderizarContadores(){
+  itemCountSpan.innerHTML = totalTareas.toString();
+  uncheckedCountSpan.innerHTML = tareasPendientes.toString();
+}
+
+function addTodo() {
+
+
+  //let nombre_tarea = `Tarea Nº ${totalTareas + 1}`
+  let nombre_tarea = prompt("Por favor ingrese el nombre de la tarea")
+  
+  const tarea = crearTarea(nombre_tarea)
+  if (tarea != undefined){
+    // actualizo el contador de tareas
+    totalTareas++;
+    tareasPendientes++;
+  }
+  // Agrego la tarea a la lista
+  list.appendChild(tarea)
+  renderizarContadores();
+
 }
